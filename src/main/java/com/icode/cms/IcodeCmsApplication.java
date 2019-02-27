b136741e18b3d47d42e8befaebede359cfcafb9a
xiachong <88388037@suningcultural.com>
2019-02-27 18:27:40 +0800

    初始化

---
+++ src/main/java/com/icode/cms/IcodeCmsApplication.java
@@ -0,0 +1,13 @@
+package com.icode.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcodeCmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcodeCmsApplication.class, args);
+	}
+
+}
