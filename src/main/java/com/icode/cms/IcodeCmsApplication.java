9ff6ef6119f78f31c469e529675d200631e92247
xiachong <88388037@suningcultural.com>
2019-06-01 16:28:27 +0800

    修改ApplicationListener加载方式

--- src/main/java/com/icode/cms/IcodeCmsApplication.java
+++ src/main/java/com/icode/cms/IcodeCmsApplication.java
@@ -1,13 +1,16 @@
 package com.icode.cms;
 
+import com.icode.cms.common.listener.ApplicationInitListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IcodeCmsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(IcodeCmsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication springApplication = new SpringApplication(IcodeCmsApplication.class);
+        springApplication.addListeners(new ApplicationInitListener());
+        springApplication.run(args);
+    }
 
 }
